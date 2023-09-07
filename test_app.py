6037574712be75a3075fa0d14944564a2169bc27
Nataphan-Rak <nataphandeark@gmail.com>
2023-09-07 15:01:58 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,12 @@
-def func(x):
+def t_func(x):
     x = " ".join([i[::-1] for i in x.split() ])
     return x
 
 def test_answer():
-    assert func("Let's take LeetCode contest") == "s'teL ekat edoCteeL tsetnoc"
-    assert func("tan tana tannn") == "nat anat nnnat"    
-    assert func("buddy kung") == "yddub gunk"
-    assert func("L0L 1S Tr4ash") == "L0L S1 hs4rT"
+    assert t_func("Let's take LeetCode contest") == "s'teL ekat edoCteeL tsetnoc"
+    assert t_func("tan tana tannn") == "nat anat nnnat"    
+    assert t_func("buddy kung") == "yddub gnuk"
+    assert t_func("L0L 1S Tr4sh") == "L0L S1 hs4rT"
      
+# test_answer()
+print(t_func("L0L 1S Tr4sh"))
